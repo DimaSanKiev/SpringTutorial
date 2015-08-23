09595c305690c189dd2dc453b4f4a8f991b91b4c
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2015-08-23 11:43:21 +0300

    ApplicationContextAware

--- src/ua/burdyga/spring/DrawingApp.java
+++ src/ua/burdyga/spring/DrawingApp.java
@@ -5,8 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
     public static void main(String[] args) {
-//        Triangle triangle = new Triangle();
-//        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Triangle triangle = (Triangle) context.getBean("triangle");
         triangle.draw();
