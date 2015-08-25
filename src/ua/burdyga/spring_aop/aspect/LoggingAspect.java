c3ea257da284b57f7e5f86beb6a05beb5c0e79b1
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2015-08-25 20:13:12 +0300

    Pointcuts and Wildcards expressions

--- src/ua/burdyga/spring_aop/aspect/LoggingAspect.java
+++ src/ua/burdyga/spring_aop/aspect/LoggingAspect.java
@@ -2,12 +2,22 @@ package ua.burdyga.spring_aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(public String getName())")
+    @Before("allGetters()")
     public void loggingAdvice() {
         System.out.println("Advice run. Get method called");
+    }
+
+    @Before("allGetters()")
+    public void secondAdvice() {
+        System.out.println("Second advice executed");
+    }
+
+    @Pointcut("execution(* get*())")
+    public void allGetters() {
     }
 }
