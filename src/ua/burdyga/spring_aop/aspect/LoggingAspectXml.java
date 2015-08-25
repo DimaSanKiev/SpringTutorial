856b9b92158e52f72d4e1a2307eaa3771aadce5d
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2015-08-26 02:07:39 +0300

    AOP XML configuration

---
+++ src/ua/burdyga/spring_aop/aspect/LoggingAspectXml.java
@@ -0,0 +1,22 @@
+package ua.burdyga.spring_aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class LoggingAspectXml {
+
+    public Object myAroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
+        Object returnValue = null;
+
+        try {
+            System.out.println("Before advice xml");
+            returnValue = proceedingJoinPoint.proceed();
+            System.out.println("After returning xml");
+        } catch (Throwable throwable) {
+            System.out.println("After throwing xml");
+        }
+
+        System.out.println("After finally xml");
+        return returnValue;
+    }
+
+}
